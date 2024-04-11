a3bcf10ab8399a4cd82d155f003afd692474187c
mobicham <hicham@mobiuslabs.com>
2024-04-11 11:13:52 +0000

    add padding support

--- hqq/core/torch_lowbit.py
+++ hqq/core/torch_lowbit.py
@@ -2,9 +2,10 @@
 #####################################################
 import torch, copy
 from torch import uint8, int32, bfloat16, nn, Tensor
+import torch.nn.functional as F
 
-from .utils import cleanup
-from .quantize import Quantizer, HQQLinear
+# from .utils import cleanup
+# from .quantize import Quantizer, HQQLinear
 
 #Adapted from: https://github.com/pytorch-labs/gpt-fast/blob/main/quantize.py
 #WARNING: These scales/zeros are in HQQ format: W_r = ((W_q - zeros)*scales).reshape(shape)
@@ -18,7 +19,7 @@ class HQQLinearTorchWeightOnlynt4(torch.nn.Module):
         device: str = "cuda",
         initialize: bool = True,
         inner_k_tiles=8, 
-        padding=False,
+        padding=True,
     ):
         super().__init__()
 
@@ -47,56 +48,12 @@ class HQQLinearTorchWeightOnlynt4(torch.nn.Module):
             self.initialize()
 
 
-    def quantize(self, W: Tensor, weight_quant_params: dict, scale_quant_params=dict | None, zero_quant_params=dict | None, offload_meta=False):
-
-        W_q, meta = Quantizer.quantize(W, **weight_quant_params, device=self.device, compute_dtype=self.compute_dtype, bitpack=False)
-
-        #ToDO: meta quantization
-
-        return W_q, meta 
-
-
-    #TODO: move this to utils
-    @torch.no_grad()
-    def reshape_meta_axis1(self, meta_tensor, new_group_size, shape):
-        meta_tensor = meta_tensor.repeat([1, shape[1]]).reshape(shape)
-        meta_tensor = torch.mean(meta_tensor.reshape([-1, new_group_size]), axis=1, keepdim=True)
-        return meta_tensor
-
-
-    @torch.no_grad()
-    def process_hqq_quants(self, W_q, meta):
-
-        scales = meta['scale']
-        zeros  = meta['zero']
-        shape  = meta['shape']
-
-        if(meta["packing"] is not None):
-            W_q = Quantizer.unpack[meta['packing']](W_q)
-
-        if(self.groupsize is None):
-            self.groupsize = 128
-            W_q    = W_q.reshape([-1, self.groupsize])
-            scales = self.reshape_meta_axis1(scales, self.groupsize, shape)
-            zeros  = self.reshape_meta_axis1(zeros,  self.groupsize, shape)
-
-        self.shape         = shape
-        self.in_features   = shape[1]
-        self.out_features  = shape[0]
-        
-        if(self.padding):
-            self.origin_in_features = self.in_features
-            self.in_features        = self.find_multiple(self.in_features, 1024)
-
-        W_q_torch, scales_torch, zeros_torch = self.hqq_quants_to_torch_quants(W_q=W_q, scales=scales, zeros=zeros, shape=shape, nbits=self.nbits)
-        self.weight_int4pack  = torch.ops.aten._convert_weight_to_int4pack(W_q_torch, self.inner_k_tiles)
-        self.scales_and_zeros = self.pack_scales_and_zeros(scales_torch, zeros_torch)
-
-        del W_q_torch, scales_torch, zeros_torch
-        torch.cuda.empty_cache()
-
-
+    ###################### Initializers ###################### 
     def initialize_with_hqq_quants(self, W_q, meta, bias=None):
+
+        self.padding = False #Force padding off, a bit tricky to post-pad with grouping
+
+        self.set_shape(meta['shape'])
         self.process_hqq_quants(W_q, meta)
         self.bias   = bias
         self.ready  = True
@@ -107,8 +64,14 @@ class HQQLinearTorchWeightOnlynt4(torch.nn.Module):
 
     def initialize(self):
         if self.linear_layer is not None:
+
+            W = self.linear_layer.weight.data
+            self.set_shape(W.shape)
+
+            if(self.in_features_diff>0):
+                W =  F.pad(W, pad=(0, self.in_features_diff), value=0)
             
-            W_q, meta = self.quantize(self.linear_layer.weight.data, **self.quant_config)
+            W_q, meta = self.quantize(W, **self.quant_config)
             self.process_hqq_quants(W_q, meta)
             del W_q, meta
 
@@ -127,16 +90,69 @@ class HQQLinearTorchWeightOnlynt4(torch.nn.Module):
 
         return self
 
+    ###################### Quantize/packing ######################
+
+    def quantize(self, W: Tensor, weight_quant_params: dict, scale_quant_params=dict | None, zero_quant_params=dict | None, offload_meta=False):
+
+        W_q, meta = Quantizer.quantize(W, **weight_quant_params, device=self.device, compute_dtype=self.compute_dtype, bitpack=False)
+
+        #ToDO: meta quantization
+
+        return W_q, meta 
+
+
+    #TODO: move these to utils
+    @torch.no_grad()
+    def reshape_meta_axis1(self, meta_tensor, new_group_size, shape):
+        meta_tensor = meta_tensor.repeat([1, shape[1]]).reshape(shape)
+        meta_tensor = torch.mean(meta_tensor.reshape([-1, new_group_size]), axis=1, keepdim=True)
+        return meta_tensor
+
     def find_multiple(self, n: int, k: int) -> int:
         if n % k == 0: return n
         return n + k - (n % k)
 
+    def set_shape(self, shape):
+        self.shape         = shape
+        self.in_features   = shape[1]
+        self.out_features  = shape[0]
+        
+        self.origin_in_features = self.in_features
+        if(self.padding):
+            self.in_features = self.find_multiple(self.in_features, 1024)
+        
+        self.in_features_diff = self.in_features - self.origin_in_features
+
+    @torch.no_grad()
+    def process_hqq_quants(self, W_q, meta):
+
+        scales = meta['scale']
+        zeros  = meta['zero']
+        shape  = meta['shape']
+
+        if(meta["packing"] is not None):
+            W_q = Quantizer.unpack[meta['packing']](W_q)
+
+        if(self.groupsize is None):
+            self.groupsize = 128
+            W_q    = W_q.reshape([-1, self.groupsize])
+            scales = self.reshape_meta_axis1(scales, self.groupsize, shape)
+            zeros  = self.reshape_meta_axis1(zeros,  self.groupsize, shape)
+
+
+        W_q_torch, scales_torch, zeros_torch = self.hqq_quants_to_torch_quants(W_q=W_q, scales=scales, zeros=zeros, shape=shape, nbits=self.nbits)
+        self.weight_int4pack  = torch.ops.aten._convert_weight_to_int4pack(W_q_torch, self.inner_k_tiles)
+        self.scales_and_zeros = self.pack_scales_and_zeros(scales_torch, zeros_torch)
+
+        del W_q_torch, scales_torch, zeros_torch
+        torch.cuda.empty_cache()
+
     @torch.no_grad()
     def hqq_quants_to_torch_quants(self, W_q: Tensor, scales: Tensor, zeros: Tensor, shape, nbits=4):
 
-        W_q       = W_q.to(dtype=self.compute_dtype, device=self.device)
-        scales    = scales.to(dtype=self.compute_dtype, device=self.device)
-        zeros     = zeros.to(dtype=self.compute_dtype, device=self.device)
+        W_q    = W_q.to(dtype=self.compute_dtype, device=self.device)
+        scales = scales.to(dtype=self.compute_dtype, device=self.device)
+        zeros  = zeros.to(dtype=self.compute_dtype, device=self.device)
 
         max_int = 2**nbits - 1
         min_int = 0
@@ -149,6 +165,7 @@ class HQQLinearTorchWeightOnlynt4(torch.nn.Module):
 
         #group_quantize_tensor_from_qparams
         W_r  = (W_q - zeros)*scales
+
         W_q = W_r.sub(min_val).div(scales).round().clamp_(min_int, max_int).to(torch.int32).reshape(shape).contiguous()
 
         #group_dequantize_tensor_from_qparams
@@ -175,6 +192,8 @@ class HQQLinearTorchWeightOnlynt4(torch.nn.Module):
             .contiguous()
         )
 
+    ###################### Forward/matmul ######################
+
     @torch.jit.ignore()
     def matmul(self, x):
         origin_x_size = x.size()
@@ -184,33 +203,61 @@ class HQQLinearTorchWeightOnlynt4(torch.nn.Module):
         c = c.reshape(new_shape)
         return c
 
-    #TODO 
+    #TODO without matmul
     def dequantize(self):
-        return self.matmul(torch.eye(self.out_features, dtype=self.compute_dtype, device=self.device)).t()
-
+        return self.matmul(torch.eye(self.in_features, dtype=self.compute_dtype, device=self.device))[:self.origin_in_features].t()
 
     #TODO: backward
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x.to(self.compute_dtype)
-        if self.padding:
-            x = torch.nn.functional.F.pad(x, pad=(0, self.in_features - self.origin_in_features))
+
+        if self.in_features_diff > 0:
+            x = F.pad(x, pad=(0, self.in_features_diff))
 
         out = self.matmul(x)
+
         if(self.bias is not None):
             out += self.bias
         return out
 
 
+###################### Patching ######################
 def patch_HQQLinear_to_HQQLinearTorchWeightOnlynt4(layer, patch_params):
 
-	new_layer = layer
+    new_layer = layer
 
-	if(type(layer) is HQQLinear):
-		new_layer  = HQQLinearTorchWeightOnlynt4(None, quant_config=layer.quant_config, compute_dtype=layer.compute_dtype, device=layer.device, del_orig=False, initialize=False)
-		new_layer.initialize_with_hqq_quants(layer.W_q, layer.meta, layer.bias)
+    if(type(layer) is HQQLinear):
+        new_layer  = HQQLinearTorchWeightOnlynt4(None, quant_config=layer.quant_config, compute_dtype=layer.compute_dtype, device=layer.device, del_orig=False, initialize=False, padding=False)
+        new_layer.initialize_with_hqq_quants(layer.W_q, layer.meta, layer.bias)
 
-	return new_layer
+    return new_layer
 
 def replace_with_torchInt4(model):
-	model.base_class.patch_linearlayers(model, patch_HQQLinear_to_HQQLinearTorchWeightOnlynt4, dict([(k, None) for k in model.base_class.get_linear_tags()]))
-	cleanup()
+    model.base_class.patch_linearlayers(model, patch_HQQLinear_to_HQQLinearTorchWeightOnlynt4, dict([(k, None) for k in model.base_class.get_linear_tags()]))
+    cleanup()
+
+
+#Force requantize, mainly to check if the padding with int4mm is faster
+def patch_HQQLinear_to_HQQLinearTorchWeightOnlynt4_force_requantize(layer, patch_params):
+
+    new_layer = layer
+
+    if(type(layer) is HQQLinear):
+        #Create dummy linear layer to store dequantize weights
+        dummy_linear = torch.nn.Linear(1, 1, bias=False)
+        dummy_linear.weight.data = layer.dequantize()
+
+        #Disable optimizer on already dequantized weights
+        quant_config = layer.quant_config
+        quant_config['weight_quant_params']['optimize'] = False
+
+        new_layer = HQQLinearTorchWeightOnlynt4(dummy_linear, quant_config=quant_config, compute_dtype=layer.compute_dtype, device=layer.device, del_orig=True, initialize=True, padding=True)
+        
+        del layer 
+        cleanup()
+  
+    return new_layer
+
+def replace_with_torchInt4_force_requantize(model):
+    model.base_class.patch_linearlayers(model, patch_HQQLinear_to_HQQLinearTorchWeightOnlynt4_force_requantize, dict([(k, None) for k in model.base_class.get_linear_tags()]))
+    cleanup()
